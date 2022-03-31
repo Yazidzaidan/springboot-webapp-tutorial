bf813005643b796f86c4e8b294494a504d80ec4d
Yazidzaidan <yazidzaidanz@gmail.com>
2022-03-31 10:22:57 +0700

    add git

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -13,23 +13,44 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.apache.catalina.User;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @PageTitle("Contacts | Vaadin CRM")
 @Route(value = "")
 public class ListView extends VerticalLayout {
     Grid<Contact> grid = new Grid<>(Contact.class);
     TextField filterText = new TextField();
+    ContactForm form;
 
     public ListView() {
         addClassName("list-view");
         setSizeFull();
 
         cofigureGrid();
+        configureForm();
 
         add(
           getToolbar(),
-          grid
+          getContent()
         );
+    }
+
+    private Component getContent() {
+        HorizontalLayout content = new HorizontalLayout(grid, form);
+        content.setFlexGrow(2, grid);
+        content.setFlexGrow(1, form);
+        content.addClassName("content");
+        content.setSizeFull();
+
+        return content;
+    }
+
+    private void configureForm() {
+        form = new ContactForm(Collections.emptyList(),Collections.emptyList());
+        form.setWidth("25em");
     }
 
     private Component getToolbar() {
